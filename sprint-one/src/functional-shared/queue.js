6f5979bd13ad4e0271a993420cbb8612015152a8
Hack Reactor Student <student@hackreactor.com>
2017-08-23 16:01:46 -0700

    implemented functional-shared pattern

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,38 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.count = 0;
+  someInstance.enqueue = queueMethods.enqueue;
+  someInstance.dequeue = queueMethods.dequeue;
+  someInstance.size = queueMethods.size;
+  
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.count++;
+    this.storage[this.count] = value;
+  },
+  dequeue: function() {
+    var results = this.storage[1];
+    for (var key = 1; key <= this.count; key++) {
+      if (key !== this.count) {
+        this.storage[key] = this.storage[key + 1];
+      } else { 
+        delete this.storage[key];
+      }
+    }
+    if (this.count > 0) {
+      this.count--;
+    }
+    return results;
+  },
+  size: function() {
+    return this.count;
+  }
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.count = 0;
+  someInstance.push = stackMethods.push;
+  someInstance.pop = stackMethods.pop;
+  someInstance.size = stackMethods.size;
+
+  return someInstance;
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  push: function(value) {
+    this.count++;
+    this.storage[this.count] = value;
+  },
+  pop: function () {
+    var result = this.storage[this.count];
+    delete this.storage[this.count];
+    if (this.count > 0) {
+      this.count--;
+    }
+    return result;
+  },
+  size: function () {
+    return this.count;
+  }
+};
