a3329b6a8c169a25ec406a4e30cee9b1a7d5f6dd
Hack Reactor Student <student@hackreactor.com>
2017-08-23 16:26:11 -0700

    implement pseudoclassical pattern

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,28 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.count = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.count++;
+  this.storage[this.count] = value;
+};
 
+Queue.prototype.dequeue = function () {
+  var results = this.storage[1];
+  for (var key = 1; key <= this.count; key++) {
+    if (key !== this.count) {
+      this.storage[key] = this.storage[key + 1];
+    } else { 
+      delete this.storage[key];
+    }
+  }
+  if (this.count > 0) {
+    this.count--;
+  }
+  return results;
+};
+
+Queue.prototype.size = function() {
+  return this.count;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,26 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
+  this.storage = {};
+  this.count = 0;
+  
 };
 
 
+
+Stack.prototype.push = function(value) {
+  this.count++;
+  this.storage[this.count] = value;
+};
+
+Stack.prototype.pop = function () {
+  var result = this.storage[this.count];
+  delete this.storage[this.count];
+  if (this.count > 0) {
+    this.count--;
+  }
+  return result;
+};
+
+Stack.prototype.size = function () {
+  return this.count;
+};
