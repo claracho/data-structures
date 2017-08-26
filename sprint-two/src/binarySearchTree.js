e1eb2e0b28f1828c3d1f32608f89b0c75617ed33
Hack Reactor Student <student@hackreactor.com>
2017-08-25 22:11:13 -0700

    implemented BST using functional class instantiation pattern

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,73 @@
 var BinarySearchTree = function(value) {
+  var BST = BSTNode(value);
+
+   
+  BST.insert = function(value) {
+    var currentNode = arguments[1] || BST;
+    // if value is less than currentNode.value
+    if (value < currentNode.value) {
+      // if currentNode.left exists, then recurse currentNode.insert()
+      if (currentNode.left) {
+        BST.insert(value, currentNode.left);
+      } else { // else, currentNode.left = BSTNode(value);
+        currentNode.left = BSTNode(value);
+      }
+    } else if (value > currentNode.value) { // if value is greater than currentNode.value
+      // if currentNode.right exists, then recurse currentNode.insert()
+      if (currentNode.right) {
+        BST.insert(value, currentNode.right);
+      } else { // else, currentNode.right = BSTNode(value);
+        currentNode.right = BSTNode(value);
+      }
+    }
+  };
+
+  BST.contains = function(value) {
+    var currentNode = arguments[1] || BST;
+    if (value === currentNode.value) {
+      return true;
+    } else if (value < currentNode.value) {
+      if (currentNode.left === null) {
+        return false;
+      }
+      return BST.contains(value, currentNode.left);
+    } else if (value > currentNode.value) {
+      if (currentNode.right === null) {
+        return false;
+      }
+      return BST.contains(value, currentNode.right);
+    }
+  };
+
+  BST.depthFirstLog = function(cb) {
+    var currentNode = arguments[1] || BST;
+    temp = cb;
+    
+    temp(currentNode.value);  
+  
+    if (currentNode.left) {
+      BST.depthFirstLog(temp, currentNode.left);
+    }
+    if (currentNode.right) {
+      BST.depthFirstLog(temp, currentNode.right);
+    }
+  };
+
+  return BST;
+};
+
+var BSTNode = function(value) {
+  var BSTNode = {};
+  
+  BSTNode.value = value;
+  BSTNode.right = null;
+  BSTNode.left = null;
+
+  return BSTNode;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+ 
